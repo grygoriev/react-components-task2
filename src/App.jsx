f03ae10dc489c7bf689313468787d4e2ce64e003
grygoriev <vovagrigoriev@gmail.com>
2024-12-14 13:04:40 +0200

    init app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,73 @@
-import ReactLogo from './assets/react.svg?react';
+import styles from './App.module.css';
+import data from './assets/data.json';
+import { useState } from 'react';
 
 export const App = () => {
+	const [steps, setSteps] = useState(data);
+	const [activeIndex, setActiveIndex] = useState(0);
+
+	const clickPrev = () => {
+		setActiveIndex(activeIndex - 1);
+	};
+	const clickNext = () => {
+		setActiveIndex(activeIndex + 1);
+	};
+	const clickStartFromBegin = () => {
+		setActiveIndex(0);
+	};
+
+	const isFirstStep = activeIndex === 0;
+	const isLastStep = activeIndex === steps.length - 1;
+
+	console.log(steps);
+
 	return (
-		<div className="container">
-			<h1>Hello World!</h1>
-			<ReactLogo />
-			<hr />
-			<span className="span-year">{new Date().getFullYear()}</span>
+		<div className={styles.container}>
+			<div className={styles.card}>
+				<h1>Инструкция по готовке пельменей</h1>
+				<div className={styles.steps}>
+					<div className={styles['steps-content']}>
+						{steps[activeIndex].content}
+					</div>
+					<ul className={styles['steps-list']}>
+						{steps.map((item, index) => (
+							<li
+								className={
+									styles['steps-item'] +
+									' ' +
+									(index <= activeIndex ? styles.done : '') +
+									' ' +
+									(index === activeIndex ? styles.active : '')
+								}
+								key={item.id}
+							>
+								<button
+									className={styles['steps-item-button']}
+									onClick={() => setActiveIndex(index)}
+								>
+									{index + 1}
+								</button>
+								{item.title}
+							</li>
+						))}
+					</ul>
+					<div className={styles['buttons-container']}>
+						<button
+							className={styles.button}
+							disabled={isFirstStep}
+							onClick={clickPrev}
+						>
+							Назад
+						</button>
+						<button
+							className={styles.button}
+							onClick={isLastStep ? clickStartFromBegin : clickNext}
+						>
+							{isLastStep ? 'Начать сначала' : 'Далее'}
+						</button>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
